4a4fc0ad54f53b1d45dfe52bdc664de1d8f13745
Akram Khan <akram.iitbombay@gmail.com>
2022-01-31 23:45:27 +0530

    reg

---
+++ amplify-tech website content/homescript.js
@@ -0,0 +1,26 @@
+  var x=0;                            //0 means closed 1 open
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  if( w<=500){ x=1;}
+  else{}
+
+function openNav() {
+    if (x==0) {
+      document.getElementById("mySidebar").style.width = "200px";
+      document.getElementById("main").style.marginLeft = "200px";
+      x=1;
+    }
+    else{
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+      x=0;
+    }
+}
+function myhide(button) {
+  var x = button.parentNode.querySelector("div");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
